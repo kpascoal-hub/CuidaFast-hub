7638e30e026d6d81072d00ccfba724cd280097a9
kpascoal-hub <pascoalmatheus01@gmail.com>
2025-05-15 15:59:43 -0300

    Update cadastro.js

--- front-end/JS/cadastro.js
+++ front-end/JS/cadastro.js
@@ -1,25 +1,35 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js";
 
-// Substitua pelos dados REAIS do Firebase
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyC_ZTNiWWOD1wk8nnbkIPZXlakr0ezQnrc",
   authDomain: "cuidafasta-hub.firebaseapp.com",
   projectId: "cuidafasta-hub",
-  storageBucket: "cuidafasta-hub.firebasestorage.app",
+  storageBucket: "cuidafasta-hub.appspot.com",
   messagingSenderId: "376865099896",
   appId: "1:376865099896:web:6e2547c4731a6626965d8d",
   measurementId: "G-KDHEPLER7P"
 };
 
-// Initialize Firebase
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const btnGoogle = document.getElementById("btnGoogle");
+
+btnGoogle.addEventListener("click", () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+      console.log("Usuário autenticado:", user);
+    })
+    .catch((error) => {
+      console.error("Erro no login com Google:", error);
+    });
+});
