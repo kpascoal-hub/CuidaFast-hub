c5c41d37406d6ebce5ccd14c98b4d7bc8e7f5af4
kpascoal-hub <pascoalmatheus01@gmail.com>
2025-05-15 18:46:49 -0300

    Update cadastro.js

--- front-end/JS/cadastro.js
+++ front-end/JS/cadastro.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-auth.js";
 
-// Configuração do Firebase
+// Config Firebase (com os dados do seu projeto novo)
 const firebaseConfig = {
-  apiKey: "AIzaSyC_ZTNiWWOD1wk8nnbkIPZXlakr0ezQnrc",
-  authDomain: "cuidafast-hub.firebaseapp.com",
-  projectId: "cuidafast-hub",
-  storageBucket: "cuidafast-hub.appspot.com",
-  messagingSenderId: "376865099896",
-  appId: "1:376865099896:web:6e2547c4731a6626965d8d"
+  apiKey: "AIzaSyBsiC8RaCd-6bwuThixa1ZxFkK4JhHgfjk",
+  authDomain: "cuidafast-hub-af250.firebaseapp.com",
+  projectId: "cuidafast-hub-af250",
+  storageBucket: "cuidafast-hub-af250.firebasestorage.app",
+  messagingSenderId: "263800638065",
+  appId: "1:263800638065:web:9b655c9d3e3acea160e9d0",
+  measurementId: "G-701M8B5CZC"
 };
 
 // Inicializa Firebase
@@ -16,7 +17,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-// Evento para login com Google
 const btnGoogle = document.getElementById("btnGoogle");
 if (btnGoogle) {
   btnGoogle.addEventListener("click", () => {
