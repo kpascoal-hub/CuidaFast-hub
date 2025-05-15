f02c7b511ef4bc6fa725a97ff89f48cdcf508d28
kpascoal-hub <pascoalmatheus01@gmail.com>
2025-05-15 09:42:28 -0300

    Update cadastro.js

--- front-end/JS/cadastro.js
+++ front-end/JS/cadastro.js
@@ -1,17 +1,14 @@
 // Importações do Firebase
+import { GoogleAuthProvider } from "firebase/auth";
+const provider = new GoogleAuthProvider();
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { getAuth } from "firebase/auth";
 
-// Configuração do seu projeto Firebase
-const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_ID",
-  appId: "SEU_APP_ID"
-};
-
+const auth = getAuth();
+auth.languageCode = 'it';
+// To apply the default browser preference instead of explicitly setting it.
+// auth.useDeviceLanguage();
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
