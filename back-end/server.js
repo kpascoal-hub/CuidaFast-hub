36cfc53bb10959339b0ada3079dc41cac3803f4f
kpascoal-hub <pascoalmatheus01@gmail.com>
2025-08-07 10:44:51 -0300

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -2,17 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Servir a pasta front-end com o prefixo /front-end
 app.use('/front-end', express.static(path.join(__dirname, '..', 'front-end')));
 
-// Rota raiz para servir o index.html (na pasta pai da front-end)
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
 const port = 3000;
 
-// Escutar na porta 3000 e em todas as interfaces da máquina (0.0.0.0)
 app.listen(port, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
