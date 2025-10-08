9f80611f4d7aa8ccfb73cf26b038e6fee8e66792
cuidafast-sketch <cuidafast@gmail.com>
2025-10-08 15:20:22 -0300

    Update FirebaseConfig.js

--- back-end/config/FirebaseConfig.js
+++ back-end/config/FirebaseConfig.js
@@ -1,11 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBsiC8RaCd-6bwuThixa1ZxFkK4JhHgfjk",
   authDomain: "cuidafast-hub-af250.firebaseapp.com",
@@ -15,7 +7,5 @@ const firebaseConfig = {
   appId: "1:263800638065:web:9b655c9d3e3acea160e9d0",
   measurementId: "G-701M8B5CZC"
 };
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
