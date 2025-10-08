89fe1df89877cdf5ae0ee99eba6c36c38397be98
cuidafast-sketch <cuidafast@gmail.com>
2025-10-08 15:18:19 -0300

    Update FirebaseConfig.js

--- back-end/config/FirebaseConfig.js
+++ back-end/config/FirebaseConfig.js
@@ -17,5 +17,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
